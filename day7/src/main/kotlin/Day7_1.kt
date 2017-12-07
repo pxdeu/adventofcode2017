cf24a3b2d23b6a0273ee2d50d488da08fe30f466
pxdeu <25555201+pxdeu@users.noreply.github.com>
2017-12-07 16:58:21 +0100

    solution for day 7 part 1

---
+++ day7/src/main/kotlin/Day7_1.kt
@@ -0,0 +1,26 @@
+import common.readInput
+
+fun main(args: Array<String>) {
+
+    val regex = "(\\w+)\\s\\((\\d+)\\)".toRegex()
+
+    val nodes = mutableListOf<Node>()
+    readInput().forEachLine {
+        val data = it.split("->")
+        val match = regex.matchEntire(data[0].trim()) ?: throw RuntimeException("input does not match expected format")
+        if (match.groups.size != 3) {
+            throw RuntimeException("input did not contain name and weight")
+        }
+        val (_, name, weight) = match.groups.map {it!!.value }
+        val children = if (data.size > 1) {
+            data[1].split(",").map { it.trim() }
+        } else {
+            listOf()
+        }
+        nodes.add(Node(name, weight, children))
+    }
+    val allChildren = nodes.flatMap { it.children }.toSet()
+    println(nodes.find { !allChildren.contains(it.name) })
+}
+
+data class Node(val name: String, val weight: String, val children: List<String>)
